ab4d6d1dc37521fbffae73b752a9718b4546b8a0
k.borodulin.stc <k.borodulin.stc@innopolis.university>
2019-11-12 20:03:38 +0500

    Тестовые классы

---
+++ src/test/java/ru/innopolis/dz_2/task_1/App.java
@@ -0,0 +1,10 @@
+package ru.innopolis.dz_2.task_1;
+
+import org.junit.Test;
+
+public class App {
+    @Test
+    public void testApp() {
+
+    }
+}
---
+++ src/test/java/ru/innopolis/dz_2/task_2/App.java
@@ -0,0 +1,10 @@
+package ru.innopolis.dz_2.task_2;
+
+import org.junit.Test;
+
+public class App {
+    @Test
+    public void testApp() {
+
+    }
+}
