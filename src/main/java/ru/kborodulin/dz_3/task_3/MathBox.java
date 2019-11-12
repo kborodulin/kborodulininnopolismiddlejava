7f0042c51e210a63c0fd80af855fd98d91ff351a
Borodulin Kirill <kborodulin@diasoft.ru>
2019-11-12 18:38:53 +0500

    dz5.task3

--- src/main/java/ru/kborodulin/dz_3/task_3/MathBox.java
+++ src/main/java/ru/kborodulin/dz_3/task_3/MathBox.java
@@ -2,44 +2,31 @@ package ru.kborodulin.dz_3.task_3;
 
 import ru.kborodulin.dz_3.task_1.ServiceNumber;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
-public class MathBox<T extends ObjectBox> {
-    private Set<Number> numberSet = new HashSet<>();
+public class MathBox<T extends Number> extends ObjectBox<T> {
+    private Set<T> numberSet = new HashSet<>();
 
-    public Set<Number> getNumberSet() {
+    public Set<T> getNumberSet() {
         return numberSet;
     }
 
-    public MathBox(Number[] arrNumbers) {
-        for (Number number : arrNumbers) {
+    public MathBox(T[] arrNumbers) {
+        for (T number : arrNumbers) {
             numberSet.add(number);
         }
     }
 
     public double summator() {
         double totalSum = 0d;
-        for (Number number : numberSet) {
+        for (T number : numberSet) {
             totalSum += number.doubleValue();
         }
         return totalSum;
     }
 
-    public void splitter(Number divider) {
-        Set<Number> numberSetTmp = new HashSet<>();
-        for (Iterator<Number> iterator = numberSet.iterator(); iterator.hasNext(); ) {
-            Number element = iterator.next();
-            numberSetTmp.add(ServiceNumber.checkNumber(element, divider));
-            iterator.remove();
-        }
-        numberSet.addAll(numberSetTmp);
-    }
-
     public void deleteValueCollectionForInteger() {
-        for (Iterator<Number> iterator = numberSet.iterator(); iterator.hasNext(); ) {
+        for (Iterator<T> iterator = numberSet.iterator(); iterator.hasNext(); ) {
             Number element = iterator.next();
             if (element instanceof Integer) {
                 iterator.remove();
@@ -48,16 +35,37 @@ public class MathBox<T extends ObjectBox> {
     }
 
     @Override
+    public void addObject(T[] object) {
+        super.addObject(object);
+    }
+
+    @Override
+    public void deleteObject(T[] object) {
+        super.deleteObject(object);
+    }
+
+    public void splitter(T divider) {
+        Set<T> numberSetTmp = new HashSet<>();
+        for (Iterator<T> iterator = numberSet.iterator(); iterator.hasNext(); ) {
+            T element = iterator.next();
+            numberSetTmp.add((T) ServiceNumber.checkNumber(element, divider));
+            iterator.remove();
+        }
+        numberSet.addAll(numberSetTmp);
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        MathBox mathBox = (MathBox) o;
+        if (!super.equals(o)) return false;
+        MathBox<?> mathBox = (MathBox<?>) o;
         return Objects.equals(numberSet, mathBox.numberSet);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(numberSet);
+        return Objects.hash(super.hashCode(), numberSet);
     }
 
     @Override
---
+++ src/test/java/ru/kborodulin/dz_3/task3/MathBoxTest.java
@@ -0,0 +1,41 @@
+package ru.kborodulin.dz_3.task3;
+
+import org.junit.Test;
+import ru.kborodulin.dz_3.task_3.MathBox;
+import ru.kborodulin.dz_3.task_3.ObjectBox;
+
+public class MathBoxTest {
+    @Test
+    public void addObjectTest() {
+        Number[] arr = new Number[]{10, 30L, 20, 15, 5, 20, 20.7};
+        Object[] obj = new Object[]{10, 30L, 20, 15, 5, 20, 20.7};
+        ObjectBox<Number> numberObjectBox = new MathBox<>(arr);
+        numberObjectBox.addObject(arr);
+        // numberObjectBox.addObject(obj);
+    }
+
+    @Test
+    public void deleteObjectTest() {
+        Number[] arr = new Number[]{10, 30L, 20, 15, 5, 20, 20.7};
+        Object[] obj = new Object[]{10, 30L, 20, 15, 5, 20, 20.7};
+        ObjectBox<Number> numberObjectBox = new MathBox<>(arr);
+        numberObjectBox.addObject(arr);
+        numberObjectBox.deleteObject(arr);
+        // numberObjectBox.addObject(obj);
+    }
+
+    @Test
+    public void summatorTest() {
+        Number[] arr = new Number[]{10, 30L, 20, 15, 5, 20, 20.7};
+        ObjectBox<Number> numberObjectBox = new MathBox<>(arr);
+        System.out.println(((MathBox<Number>) numberObjectBox).summator());
+    }
+
+    @Test
+    public void dumpTest() {
+        Number[] arr = new Number[]{10, 30L, 20, 15, 5, 20, 20.7};
+        ObjectBox<Number> numberObjectBox = new MathBox<>(arr);
+        numberObjectBox.addObject(arr);
+        System.out.println(numberObjectBox.dump());
+    }
+}
